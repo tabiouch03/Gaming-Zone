81d56b434db2331816bde828c164cb0782ec6516
tabiouch03 <guerreiro.fabio@outlook.fr>
2020-08-01 20:09:38 +0200

    feature:Border-card

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,18 @@ import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
 import { FormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
+import { gameBorderCard } from "./directive/border-card.directive";
 
 import { AppComponent } from './app.component';
 import { GameComponent } from './game/game.component';
 import { GameDetailComponent } from './game/game-detail/game-detail.component';
 import { NavbarComponent } from './navbar/navbar.component';
+import { GameListComponent } from './game/game-list/game-list.component';
+import { GameNewsComponent } from './game/game-news/game-news.component';
 
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MaterialModule } from './material/material.module';
-import { GameListComponent } from './game/game-list/game-list.component';
-import { GameNewsComponent } from './game/game-news/game-news.component';
+
 @NgModule({
   declarations: [
     AppComponent,
@@ -21,6 +23,7 @@ import { GameNewsComponent } from './game/game-news/game-news.component';
     NavbarComponent,
     GameListComponent,
     GameNewsComponent,
+    gameBorderCard
   ],
   imports: [
     BrowserModule,
